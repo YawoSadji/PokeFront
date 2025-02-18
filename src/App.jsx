92a85e0f47dd09b78323f3523ed450359c8485c9
YawoSadji <yawo.sadji@gmx.com>
2025-02-18 15:13:54 -0500

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Button from 'react-bootstrap/Button';
 function App() {
   const [pokemons, setPokemons]= useState([]);
   const [loading, setLoading]= useState(true);
+  const handleAddClick = () => {
+    console.log("Button Clicked"); 
+  };
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
     .then(response => response.json())
@@ -37,7 +40,8 @@ function App() {
           <div key={index} className="col-md-3">
             <div className="card">
             <h5 className="card-titile">{pokemon.name}</h5>
-            <p className="card-text">URL: {pokemon.url}</p>
+            {/* <p className="card-text">URL: {pokemon.url}</p> */}
+            <button onClick={handleAddClick}>Add</button>
           </div>
           </div>))
         )}
