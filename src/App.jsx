aaa603b52c2b84377cdd343c1ddc809984048586
YawoSadji <yawo.sadji@gmx.com>
2025-02-25 16:17:17 -0500

    moved battle button to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState, useEffect, useReducer } from "react";
-import Carousel from 'react-bootstrap/Carousel';
-import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Button';
-const initialState = { squad: [], }
-const reducer = (state, action)=>{
-  switch (action.type){
-    case 'add':
-      if(!state.squad.includes(action.payload)){
-        return {...state, squad: [...state.squad, action.payload]}
+import Carousel from "react-bootstrap/Carousel";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+const initialState = { squad: [] };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      if (!state.squad.includes(action.payload)) {
+        return { ...state, squad: [...state.squad, action.payload] };
       }
       return state;
-    case 'remove':
-      return{...state, squad: state.squad.filter((item)=>item !== action.payload), }
+    case "remove":
+      return {
+        ...state,
+        squad: state.squad.filter((item) => item !== action.payload),
+      };
     default:
       return state;
   }
@@ -19,23 +22,29 @@ const reducer = (state, action)=>{
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  const [state, dispatch]= useReducer(reducer, initialState);
-  const [loading, setLoading]= useState(true);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(response => response.json())
-    .then(data => {
-      setPokemons(data.results);
-      setLoading(false);
-    })
-    .catch(error => console.error(error));
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemons(data.results);
+        setLoading(false);
+      })
+      .catch((error) => console.error(error));
   }, []);
-  const handleAddToSquad = ()
-  const handleRemoveFromSquad = ()
-  const isBattleButtonEnabled = state.squad.length >=2;
-  
-  return(
+  const handleAddToSquad = (pokemon) => {
+    if (state.squad.length < 6) {
+      dispatch({ type: "add", payload: pokemon });
+    }
+  };
+  const handleRemoveFromSquad = (pokemon) => {
+    dispatch({ type: "remove", payload: pokemon });
+  };
+  const isBattleButtonEnabled = state.squad.length >= 2;
+
+  return (
     <div className="container">
       <div>
         <h1>
@@ -45,37 +54,49 @@ function App() {
           </Badge>
         </h1>
       </div>
+      <div>
+        <h2>Squad Members:</h2>
+        <ul>
+          {state.squad.map((pokemon, index) => (
+            <li key={index}>{pokemon}</li>
+          ))}
+        </ul>
+        <button className="btn btn-primary" disabled={!isBattleButtonEnabled}>
+          Battle
+        </button>
+      </div>
       <div className="row">
-        {loading?(
-          <Carousel.Item> 
+        {loading ? (
+          <Carousel.Item>
             <div>Loading...</div>
           </Carousel.Item>
-          ):(pokemons.map((pokemon, index) => (
-          <div key={index} className="col-md-3">
-            <div className="card">
-            <h5 className="card-titile">{pokemon.name}</h5>
-            {state.squad.includes(pokemon.name)?
-            (
-              <button className="btn btn-danger" onClick={()=>handleRemoveFromSquad(pokemon.name)}>Remove from Squad</button>
-            ):(
-              <button className="btn btn-success" onClick={()=>handleAddToSquad(pokemon.name)}>Add To Squad</button>
-            )
-          }
-          </div>
-          </div>))
+        ) : (
+          pokemons.map((pokemon, index) => (
+            <div key={index} className="col-md-3">
+              <div className="card">
+                <h5 className="card-titile">{pokemon.name}</h5>
+                {state.squad.includes(pokemon.name) ? (
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleRemoveFromSquad(pokemon.name)}
+                  >
+                    Remove from Squad
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-success"
+                    onClick={() => handleAddToSquad(pokemon.name)}
+                  >
+                    Add To Squad
+                  </button>
+                )}
+              </div>
+            </div>
+          ))
         )}
-    </div>
-    <div>
-      <h2>Squad Members:</h2>
-      <ul>
-        {state.squad.map((pokemon, index)=>(
-          <li key={index}>{pokemon}</li>
-        ))}
-      </ul>
-      <button className="btn btn-primary" disabled={!isBattleButtonEnabled}>Battle</button>
-    </div>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
