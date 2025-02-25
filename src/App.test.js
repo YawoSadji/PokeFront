7755828be3d266aae921020e14bcabd384019c5e
YawoSadji <yawo.sadji@gmx.com>
2025-02-25 17:11:31 -0500

    added basic test

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import {
+  render,
+  screen,
+  fireEvent,
+  getByText,
+  waitFor,
+} from "@testing-library/react";
+import App from "./App";
+
+test("App should render", () => {
+  render(<App />);
+
+  expect(screen.getByText("PokeFront")).toBeInTheDocument();
+});
